668616ca9acaa098af33db1b7d36894cd6421a56
Gelei Chen <chen1474@purdue.edu>
2017-09-20 12:46:36 -0400

    update

--- hw2/hw2-2.py
+++ hw2/hw2-2.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 my_url = "https://www.cs.purdue.edu/homes/ribeirob/courses/Fall2017/data/airpollution.csv"
 
-# local_filename, headers = urllib.request.urlretrieve(my_url)
-local_filename = "airpollution.csv"
+local_filename, headers = urllib.request.urlretrieve(my_url)
+# local_filename = "airpollution.csv"
 with open(local_filename) as in_file:
     number_of_lines = 0
     matrix_x = []
@@ -24,9 +24,23 @@ with open(local_filename) as in_file:
         # matrix_x.append([temp,manu,popul,wind,percip,predays])
         matrix_x.append(line.split(",")[-6:])
 
+    #to compute the normalized_matrix_x
+    np_matrix_x = np.array(matrix_x,dtype=float)
+    matrix_m = np_matrix_x.mean(axis=0)
+    matrix_theta = np.zeros(6)
+    for index,column in enumerate(np_matrix_x.T):
+        index_sum = 0
+        for x_i_j in column:
+            index_sum += np.power(x_i_j-matrix_m[index],2)
+        matrix_theta[index] = np.sqrt(index_sum)
+
+    normalized_matrix_x = np.zeros((41,6))
+    for index,column in enumerate(np_matrix_x.T):
+        normalized_matrix_x[:,index] = (column-matrix_m[index])/matrix_theta[index]
+
     #to compute the matrix_s
     matrix_s = np.zeros((6,6))
-    for x_j in matrix_x:
+    for x_j in normalized_matrix_x:
         x_j = np.array([x_j],dtype=float).reshape(6,1)
         x_j_transpose = np.transpose(x_j)
         dot_product = np.dot(x_j,x_j_transpose)
@@ -43,21 +57,11 @@ with open(local_filename) as in_file:
     plt.plot(w_absolute_sort)
     plt.title('Eigenvalues')
     plt.savefig('hw2-2-(b).png')
-    np_matrix_x = np.array(matrix_x,dtype=float)
+
     variance_of_matrix_x = np.var(np_matrix_x,axis=0)
     print("variance of different column in original csv:\n\t{}\n".format(variance_of_matrix_x))
     sum_of_variance_of_matrix_x = np.sum(variance_of_matrix_x)
     variance_percantage = 100 * variance_of_matrix_x / sum_of_variance_of_matrix_x
     print("variance percentage:\n\t{}\n".format(variance_percantage))
-
-
-
+    print("sum_of_variance_of_matrix_x:{}".format(sum_of_variance_of_matrix_x))
     #c
-    matrix_m = np_matrix_x.mean(axis=0)
-    matrix_theta = np.zeros(6)
-    for index,column in enumerate(np_matrix_x.T):
-        index_sum = 0
-        for x_i_j in column:
-            index_sum += np.power(x_i_j-matrix_m[index],2)
-        matrix_theta[index] = np.sqrt(index_sum)
-    print(matrix_theta)
