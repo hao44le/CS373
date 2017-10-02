012f088b783ef5a290e3420af8dacab4f1055902
Gelei Chen <chen1474@purdue.edu>
2017-10-02 08:30:39 -0400

    update he3-2 for NBC

--- hw3/hw3-2.py
+++ hw3/hw3-2.py
@@ -4,18 +4,22 @@ my_url = "https://www.cs.purdue.edu/homes/ribeirob/courses/Fall2017/data/yelp2_t
 local_filename = "yelp2_train.csv"
 
 line_number = 0
-#d
+
+#1.d
 goodForGroups_set,city_set,state_set,stars_set,is_open_set,alcohol_set,noiseLevel_set,attire_set,priceRange_set,delivery_set,waiterService_set,smoking_set,outdoorSeating_set,caters_set,goodForKids_set = set(),set(),set(),set(),set(),set(),set(),set(),set(),set(),set(),set(),set(),set(),set()
 sets = [goodForGroups_set,city_set,state_set,stars_set,is_open_set,alcohol_set,noiseLevel_set,attire_set,priceRange_set,delivery_set,waiterService_set,smoking_set,outdoorSeating_set,caters_set,goodForKids_set]
 set_names = ["goodForGroups_set","city_set","state_set","stars_set","is_open_set","alcohol_set","noiseLevel_set","attire_set","priceRange_set","delivery_set","waiterService_set","smoking_set","outdoorSeating_set","caters_set","goodForKids_set"]
 
-#g
+#1.g
 numerator_factor = 1
 priceRange_dict = {'':[0,0],'1':[0,0],'2':[0,0],'3':[0,0],'4':[0,0]}
 alcohol_dict = {'none':[0,0],'full_bar':[0,0],'beer_and_wine':[0,0]}
 noiseLevel_dict = {'':[0,0],'very_loud':[0,0],'loud':[0,0],'quiet':[0,0],'average':[0,0]}
 attire_dict = {'':[0,0],'dressy':[0,0],'casual':[0,0],'formal':[0,0]}
 all_dict = {'priceRange_dict':priceRange_dict,'alcohol_dict':alcohol_dict,'noiseLevel_dict':noiseLevel_dict,'attire_dict':attire_dict}
+
+#2
+prior_count_dict = {'0':0,'1':0}
 
 with open(local_filename) as in_file:
     for line in in_file.readlines():
@@ -26,7 +30,7 @@ with open(local_filename) as in_file:
             continue
         else:
             [goodForGroups,city,state,_,_,stars,_,_,is_open,alcohol,noiseLevel,attire,priceRange,delivery,waiterService,smoking,outdoorSeating,caters,goodForKids] = line.split(",")
-            #d
+            #1.d
             goodForGroups_set.add(goodForGroups)
             city_set.add(city)
             state_set.add(state)
@@ -43,7 +47,7 @@ with open(local_filename) as in_file:
             caters_set.add(caters)
             goodForKids_set.add(goodForKids)
 
-            #g
+            #1.g
             if goodForGroups == '0':
                 priceRange_dict[priceRange] = [priceRange_dict[priceRange][0]+1,priceRange_dict[priceRange][1]]
                 alcohol_dict[alcohol] = [alcohol_dict[alcohol][0]+1,alcohol_dict[alcohol][1]]
@@ -55,13 +59,20 @@ with open(local_filename) as in_file:
                 noiseLevel_dict[noiseLevel] = [noiseLevel_dict[noiseLevel][0],noiseLevel_dict[noiseLevel][1]+1]
                 attire_dict[attire] = [attire_dict[attire][0],attire_dict[attire][1]+1]
 
-#d
+            #2
+            prior_count_dict[goodForGroups] += 1
+
+#1.d
 # total_number_of_distinct_values = 0
 # for index,m_set in enumerate(sets):
     # print("{} {}:{}".format(set_names[index],len(m_set),m_set))
     # total_number_of_distinct_values += len(m_set)
 # print("total_number_of_distinct_values:{}".format(total_number_of_distinct_values))
 
-#g
+#1.g
 # for key in all_dict:
 #     print("{}:{}".format(key,all_dict[key]))
+
+#2
+for prior_count in prior_count_dict:
+    print("P({})=:{}".format(prior_count,prior_count_dict[prior_count] / float(line_number)))
