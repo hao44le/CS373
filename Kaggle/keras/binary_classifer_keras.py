1daab2ca5e78525ea586d7ce8a295e809e905c3b
Gelei Chen <gelei@aisense.com>
2017-11-06 15:43:50 -0500

    change the import

--- Kaggle/keras/binary_classifer_keras.py
+++ Kaggle/keras/binary_classifer_keras.py
@@ -7,7 +7,10 @@
 import numpy
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+try:
+    from sklearn.model_selection import train_test_split
+except:
+    from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
