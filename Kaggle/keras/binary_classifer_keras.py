a6500e2bff52bf376f2fdc246ff3ce7479e80dab
Gelei Chen <gelei@aisense.com>
2017-11-06 15:39:30 -0500

    rm seaborn

--- Kaggle/keras/binary_classifer_keras.py
+++ Kaggle/keras/binary_classifer_keras.py
@@ -7,7 +7,6 @@
 import numpy
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 get_ipython().magic('matplotlib inline')
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
